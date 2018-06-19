8a49bd719e1d6498ca3cabdca3c127d3705ead67
Elvis de Freitas Souza <edigitalb@gmail.com>
2018-06-19 18:50:40 -0300

    adding sources

---
+++ src/main/java/com/mageddo/Main.java
@@ -0,0 +1,12 @@
+package com.mageddo;
+
+import java.util.*;
+import java.time.*;
+
+public class Main {
+
+	public static void main(String args[]){
+		System.out.printf("%s %s%n", LocalDateTime.now(), Arrays.toString(args));
+	}
+
+}
